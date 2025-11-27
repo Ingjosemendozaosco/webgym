053b26dc66b2c7c9e0d6e03dbdf576f32b9626de
MendozaOsco <josemendozaosco@gmail.com>
2025-11-27 17:07:50 -0500

    Fix: Configura URL de API de produccion

--- gym-frontend - V1/src/app/app.config.ts
+++ gym-frontend - V1/src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
+import { apiBaseInterceptor } from './interceptors/api-base.interceptor';
 import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([apiBaseInterceptor, authInterceptor])),
     importProvidersFrom(FormsModule),
   ],
 };
